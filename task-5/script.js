87b917b965a7a260f723f99f340088b8bdd44207
ninumariya <ninumariya2518@gmail.com>
2023-03-11 14:36:29 +0400

    add javascript for the functionalities of calculator on task-5t

--- task-5/script.js
+++ task-5/script.js
@@ -1,7 +1,17 @@
-const displayBoxField = document.getElementById("displaybox");
-function displayOnScreen(val){
-    let numberEntered = val;
-    console.log(numberEntered);
-    displayBoxField.innerHTML = numberEntered;
-}
-
+function displayOnScreen(value) {
+    document.getElementById("displaybox").value += value;
+  }
+  function allclear() {
+    document.getElementById("displaybox").value = "";
+  }
+  function Delete() {
+    document.getElementById("displaybox").value = document
+      .getElementById("displaybox")
+      .value.toString()
+      .slice(0, -1);
+  }
+  function calculate() {
+    var x = document.getElementById("displaybox").value;
+    var y = eval(x);
+    document.getElementById("displaybox").value = y;
+  }
